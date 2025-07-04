f8fd9f28c75ac6f2f25a02055765d80259feabb8
AkshayaSri <maddiakshayasri@gmail.com>
2025-07-04 15:13:10 +0530

    Initial commit for house price predictor

---
+++ app.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import pickle as pk
+import streamlit as st
+
+# Load the model
+model = pk.load(open(r'C:\mlprojects\bhp.pkl', 'rb'))
+
+# Load data
+data = pd.read_csv(r'C:\mlprojects\cleaned_data.csv')
+
+# Streamlit UI
+st.header("🏠 Bangalore House Price Predictor")
+
+# User Inputs
+loc = st.selectbox('Choose the location', data['location'].unique())
+sqft = st.number_input('Enter total sqft', min_value=500)
+beds = st.number_input('Enter number of Bedrooms', min_value=1)
+balc = st.number_input('Enter number of Balconies', min_value=0)
+bath = st.number_input('Enter number of Bathrooms', min_value=1)
+
+# Prepare input
+input_data = pd.DataFrame([[loc, sqft, bath, balc, beds]],
+                           columns=['location', 'total_sqft', 'bath', 'balcony', 'bedrooms'])
+
+# Predict button
+if st.button("Predict Price"):
+    output = model.predict(input_data)
+    price = round(output[0] * 100000, 2)
+    st.success(f"🏡 Estimated Price: ₹ {price}")
